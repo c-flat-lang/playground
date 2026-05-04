4de91a3b933615a7adc5cf69b42210d87a8f6fe9
cowboy8625 <cowboy8625@yahoo.com>
2026-05-03 19:31:49 -0500

    fix path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/cb-playground",
+  basePath: "/playground",
   trailingSlash: true,
 };
 
