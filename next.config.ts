dafe33a9a6546dbe1cd78a0bf0a6ea92b3666d39
cowboy8625 <cowboy8625@yahoo.com>
2026-05-05 22:03:57 -0500

    icon

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-const isGithubPages =
-  process.env.GITHUB_PAGES === "true" || process.env.NODE_ENV === "development";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isGithubPages ? "/playground" : "",
-  assetPrefix: isGithubPages ? "/playground/" : "",
   trailingSlash: true,
 };
 
