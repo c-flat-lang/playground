027d907ba7a47db809fb6703c47114511e9ce5f4
cowboy8625 <cowboy8625@yahoo.com>
2026-06-05 16:52:08 -0500

    use webpack and no minimizing

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,25 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  webpack(config, { webpack }) {
+    config.optimization.minimize = false;
+
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(
+        /^node:/,
+        (resource: { request: string }) => {
+          resource.request = resource.request.replace(/^node:/, "");
+        },
+      ),
+    );
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: false,
+      fs: false,
+      module: false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
